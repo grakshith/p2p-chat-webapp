75e4057c89b03dcbf4ef82ea1875bd4d8b190bd8
hehaichi <hehaichi@gmail.com>
2016-05-22 00:42:34 +0530

    Added debug code

--- wsServer.py
+++ wsServer.py
@@ -4,21 +4,22 @@ clients = []
 class SimpleChat(WebSocket):
 
     def handleMessage(self):
-       for client in clients:
-          if client != self:
-             client.sendMessage(self.address + u' - ' + self.data)
+        print self.data
+        for client in clients:
+            if client != self:
+                client.sendMessage(self.address + u' - ' + self.data)
 
     def handleConnected(self):
-       print self.address, 'connected'
-       for client in clients:
-          client.sendMessage(self.address + u' - connected')
-       clients.append(self)
+        print self.address, 'connected'
+        for client in clients:
+            client.sendMessage(self.address + u' - connected')
+        clients.append(self)
 
     def handleClose(self):
-       clients.remove(self)
-       print self.address, 'closed'
-       for client in clients:
-          client.sendMessage(self.address + u' - disconnected')
+        clients.remove(self)
+        print self.address, 'closed'
+        for client in clients:
+            client.sendMessage(self.address + u' - disconnected')
 
 server = SimpleWebSocketServer('', 8080, SimpleChat)
 server.serveforever()
