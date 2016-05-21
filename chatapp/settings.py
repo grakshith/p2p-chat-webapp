0e23b6947a6a57c912d9e4170b5961770fb7ae85
hehaichi <hehaichi@gmail.com>
2016-05-21 23:27:45 +0530

    Added websocket server

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'SimpleWebSocketServer',
 ]
 
 MIDDLEWARE_CLASSES = [
--- home/views.py
+++ home/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 def chat(request):
     
-    return render(request,'templates/chat.html')
+    return render(request,'home/chat.html')
