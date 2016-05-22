af94b4a46b945c4121dd8adc0bc34c62c8f7e20d
hehaichi <hehaichi@gmail.com>
2016-05-22 22:27:53 +0530

    wsServer now works properly

--- wsServer.py
+++ wsServer.py
@@ -1,25 +1,48 @@
-from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
-
+'''
+The MIT License (MIT)
+Copyright (c) 2013 Dave P.
+'''
+import signal
+import sys
+import ssl
+from SimpleWebSocketServer import WebSocket, SimpleWebSocketServer, SimpleSSLWebSocketServer
+from optparse import OptionParser
 clients = []
 class SimpleChat(WebSocket):
 
-    def handleMessage(self):
-        print self.data
-        for client in clients:
-            if client != self:
-                client.sendMessage(self.address + u' - ' + self.data)
+   def handleMessage(self):
+      for client in clients:
+         if client != self:
+            client.sendMessage(self.address[0] + u' - ' + self.data)
 
-    def handleConnected(self):
-        print self.address, 'connected'
-        for client in clients:
-            client.sendMessage(self.address + u' - connected')
-        clients.append(self)
+   def handleConnected(self):
+      print (self.address, 'connected')
+      for client in clients:
+         client.sendMessage(self.address[0] + u' - connected')
+      clients.append(self)
 
-    def handleClose(self):
-        clients.remove(self)
-        print self.address, 'closed'
-        for client in clients:
-            client.sendMessage(self.address + u' - disconnected')
+   def handleClose(self):
+      clients.remove(self)
+      print (self.address, 'closed')
+      for client in clients:
+         client.sendMessage(self.address[0] + u' - disconnected')
 
-server = SimpleWebSocketServer('', 8080, SimpleChat)
-server.serveforever()
+
+if __name__ == "__main__":
+
+   parser = OptionParser(usage="usage: %prog [options]", version="%prog 1.0")
+   parser.add_option("--host", default='', type='string', action="store", dest="host", help="hostname (localhost)")
+   parser.add_option("--port", default=8000, type='int', action="store", dest="port", help="port (8000)")
+   
+   (options, args) = parser.parse_args()
+   cls = SimpleChat
+
+   server = SimpleWebSocketServer(options.host, options.port, cls)
+
+   def close_sig_handler(signal, frame):
+      server.close()
+      sys.exit()
+
+   signal.signal(signal.SIGINT, close_sig_handler)
+
+   server.serveforever()
