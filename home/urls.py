a85c82afedb1d3b2919af9d0f8240609072146f2
hehaichi <hehaichi@gmail.com>
2016-05-23 22:29:26 +0530

    Add user_auth urls nd views

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
-
+from django.contrib.auth.views import login
 urlpatterns=[
     url(r'^$',views.chat,name='chat'),
+    url(r'^login/',login,name='login'),
+    url(r'^logout/',views.logout,name='logout'),
+    
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 # Create your views here.
-
+from django.conf import settings
+from django.contrib.auth import logout as logot
+from django.contrib.auth import login as logn
+from django.contrib.auth import authenticate
 def chat(request):
+    if not request.user.is_authenticated:
+        return redirect('%s' % ('login/'))
+    else:        
+        return render(request,'home/chat.html')
+        
+def logout(request):
+    logot(request)
+    return render(request,'registration/logout.html')
     
-    return render(request,'home/chat.html')
+    
+
